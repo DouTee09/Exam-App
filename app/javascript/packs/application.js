b682320c5653def43f76a27ce907576d9215a283
tungtk6 <tungtk6@example.com>
2024-06-03 17:48:55 +0700

    Update page Exam

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,27 +12,10 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-// function reloadPage() {
-//     location.reload();
-// }
 document.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById('add-question').addEventListener('click', function( ) {
-        var template = document.getElementById('question-template').content;
-        document.body.appendChild(template);
+        var template = document.getElementById('question-template').content.cloneNode(true);
+        var formExam = document.getElementById('form-exam');
+        formExam.appendChild(template);
     });
-
-    // document.getElementById('questions-container').addEventListener('click', function(event) {
-    //     if (event.target && event.target.matches('.delete-button')) {
-    //         event.preventDefault();
-    //         event.target.closest('.ques-fea').remove();
-    //     }
-    // });
-    // document.getElementById('add-answer').addEventListener('click', function(event) {
-    //         event.preventDefault();
-    //         var tem = document.getElementById('answer-template').content.cloneNode(true);
-    //         document.getElementById('form-check').appendChild(tem);
-    // });
-    function addNewAnswer() {
-  
-    }
 });
