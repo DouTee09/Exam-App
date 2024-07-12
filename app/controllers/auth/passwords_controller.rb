b97fc3c19b02d7c15a990ff670cfcc9b2bb55b0c
tungtk6 <tungtk6@example.com>
2024-07-12 15:28:02 +0700

    update login, logout, forgot password, sign up

--- app/controllers/auth/passwords_controller.rb
+++ app/controllers/auth/passwords_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Auth::PasswordsController < Devise::PasswordsController
+  before_action :require_no_authentication, :only => [:edit, :update]
   # GET /resource/password/new
   # def new
   #   super
@@ -21,15 +22,9 @@ class Auth::PasswordsController < Devise::PasswordsController
   #   super
   # end
 
-  # protected
+  protected
 
   def after_resetting_password_path_for(resource)
-    debugger
-    redirect_to root_path
+    home_path
   end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
 end
--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Auth::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  # prepend_before_action :require_no_authentication, only: [:new, :create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
   def create
@@ -27,8 +27,12 @@ class Auth::SessionsController < Devise::SessionsController
 
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  def after_sign_in_path_for(resource)
+    home_path
   end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
